64bfdccbe6eadaa5847f44b7f2601d19f30b88f7
adrianogr44 <adrianopsyrt@gmail.com>
2026-03-27 15:11:09 -0300

    codigo de alteracao para dark e ligth mode (avatares diferentes)

---
+++ script.js
@@ -0,0 +1,19 @@
+const audio = new Audio("./assets/flashbang-cs_qoRhxLn.mp3");
+audio.volume = 0.2;
+
+function toggleMode() {
+    const html = document.documentElement;
+
+    html.classList.toggle('claro');
+
+    const img = document.querySelector("#profile img");
+
+    if(html.classList.contains('claro')){
+        audio.currentTime = 0;
+        audio.play();
+        img.setAttribute('src', './assets/oclin.png');
+    } else {
+        img.setAttribute('src', './assets/avatar-musgas.png');
+    }
+
+}
